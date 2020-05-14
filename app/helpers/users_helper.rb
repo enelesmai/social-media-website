5af64102139c958dc103827c9e7fac051380a6e5
enelesmai <xochitl.selene.fp@gmail.com>
2020-05-14 18:32:53 -0500

    added 'already friends' label

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,15 +1,9 @@
 module UsersHelper
   def request_friend(user)
     html = ''
-    unless current_user.requested?(user) || current_user.pending?(user) || current_user == user
+    unless current_user.requested?(user) || current_user.pending?(user) || current_user.friend?(user) || current_user == user
       html += link_to ' | Invite to friendship', friendships_path(user: user), method: :post, class: 'profile-link'
     end
-    html.html_safe
-  end
-
-  def requested_friendship(user)
-    html = ''
-    html += label_tag 'Requested friendship' if current_user.requested?(user)
     html.html_safe
   end
 
@@ -20,6 +14,13 @@ module UsersHelper
       html += link_to ' | Accept', friendship_path(friendship, user: user, confirmed: true), method: :put, class: 'profile-link'
       html += link_to ' | Decline', friendship_path(friendship, user: user, confirmed: false), method: :put, class: 'profile-link'
     end
+    html.html_safe
+  end
+
+  def label_friendship(user)
+    html = ''
+    html += label_tag 'Requested friendship' if current_user.requested?(user)
+    html += label_tag 'Already friends' if current_user.friend?(user)
     html.html_safe
   end
 end
