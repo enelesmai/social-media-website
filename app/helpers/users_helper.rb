9aad02332f9a4ff92a4be793979724effcc4c1d7
enelesmai <xochitl.selene.fp@gmail.com>
2020-05-14 18:52:10 -0500

    added different classes

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,7 @@ module UsersHelper
   def request_friend(user)
     html = ''
     unless current_user.requested?(user) || current_user.pending?(user) || current_user.friend?(user) || current_user == user
-      html += link_to ' | Invite to friendship', friendships_path(user: user), method: :post, class: 'profile-link'
+      html += link_to ' | Invite to friendship', friendships_path(user: user), method: :post, class: 'label-status'
     end
     html.html_safe
   end
@@ -11,16 +11,16 @@ module UsersHelper
     html = ''
     friendship = current_user.get_friendship(user)
     if current_user.pending?(user)
-      html += link_to ' | Accept', friendship_path(friendship, user: user, confirmed: true), method: :put, class: 'profile-link'
-      html += link_to ' | Decline', friendship_path(friendship, user: user, confirmed: false), method: :put, class: 'profile-link'
+      html += link_to ' | Accept', friendship_path(friendship, user: user, confirmed: true), method: :put, class: 'label-success'
+      html += link_to ' | Decline', friendship_path(friendship, user: user, confirmed: false), method: :put, class: 'label-danger'
     end
     html.html_safe
   end
 
   def label_friendship(user)
     html = ''
-    html += label_tag 'Requested friendship' if current_user.requested?(user)
-    html += label_tag 'Already friends' if current_user.friend?(user)
+    html += label_tag 'Requested friendship', nil, class: 'label-status' if current_user.requested?(user)
+    html += label_tag 'You are already friends!', nil, class: 'label-status' if current_user.friend?(user)
     html.html_safe
   end
 end
