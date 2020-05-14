6dbcb90e664c003848aba6266a5f38c75940f181
enelesmai <xochitl.selene.fp@gmail.com>
2020-05-14 16:48:34 -0500

    added methos to request and confirm friendship 2 rows

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,26 +11,33 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
 
   has_many :friendships
-  has_many :inverse_friendships, class_name: 'User', foreign_key: 'friend_id'
 
   def friends
-    friends_array = friendships.map { |friendship| friendship.friend if friendships.confirmed }
-    friends_array << inverse_friendships.map { |friendship| friendship.user if friendships.confirmed }
+    friends_array = friendships.map { |f| f.friend if f.confirmed }
     friends_array.compact
   end
 
-  # Users who have yet to confirm friend requests
+  # Users who have yet to confirm my friend requests
   def pending_friends
-    friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
+    friendships.map { |f| f.friend unless f.confirmed }.compact
   end
 
-  # Users who have requested to be friends
+  # Users who have requested to me to be friends
   def friends_requests
-    inverse_friendships.map { |friendship| friendship.user unless friendship.confirmed }.compact
+    Friendship.where(friend_id: id).map { |f| f.user unless f.confirmed }.compact
+  end
+
+  def request_friendship(user)
+    friendships.build(friend_id: user.id, confirmed: nil)
+    save
   end
 
   def confirm_friend(user)
-    friendship = inverse_friendships.find { |f| f.user == user }
+    friendship = Friendship.where(friend_id: id, confirmed: nil, user_id: user.id).first
+    return if friendship.nil?
+
+    # Creates the other direction friendship
+    Friendship.create(user_id: id, friend_id: user.id, confirmed: true)
     friendship.confirmed = true
     friendship.save
   end
--- db/migrate/20200514182025_create_friendships.rb
+++ db/migrate/20200514182025_create_friendships.rb
@@ -4,7 +4,7 @@ class CreateFriendships < ActiveRecord::Migration[5.2]
       t.integer :user_id, index: true
       t.integer :friend_id, index: true
       t.boolean :confirmed
-
+      
       t.timestamps
     end
   end
